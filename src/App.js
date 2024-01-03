e92348bae868eed19ee451f146f6bae0949df07f
Paulo henrique <henrique_reis@atlantico.com.br>
2024-01-02 23:07:11 -0300

    useEffect - React Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  //componentDidUpdate - executa todas vez que o component atualiza
+  useEffect(() => {
+    console.log("componentDidUpdate")
+  });
+
+  //componentDidMount - executa 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn)
+
+    //componentwillUnmount - limpeza
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn)
+    }
+  }, []);
+
+   //com dependência - executa toda vez que a dependência mudar
+   useEffect(() => {
+    console.log("C1:", counter, "C2:", counter2)
+  }, [counter, counter2]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>C1: {counter} C2: {counter2} {' '}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+2</button>
     </div>
   );
 }
