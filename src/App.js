2e5dd1c5fa096b64437f4dda5d98b2942e9f7fb9
Paulo henrique <p.henriquerm@hotmail.com>
2024-01-03 22:24:25 -0300

    useReducer - React Hooks - Parte 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useReducer } from 'react';
 import './App.css';
-import { Div } from './components/Div';
-import { AppContext } from './context/AppContext';
+
+export const globalState = {
+  title: 'O título que está no contexto',
+  body: 'O body do contexto',
+  counter: 0
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'muda':
+      console.log('Chamou muda com', action.payload)
+      return { ...state, title: action.payload };
+
+    case 'inverter':
+      console.log('Chamou inverter')
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+
+    default:
+      return {...state };
+  }
+};
 
 function App() {
+  const [state, dipatch] = useReducer(reducer, globalState);
+  const { title } = state;
+
   return (
-  <AppContext>
-    <Div />
-  </AppContext>
+    <div>
+      <h1>{title}</h1>
+      <button
+        onClick={() =>
+          dipatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-br')
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dipatch({ type: 'inverter' })}>Inverter</button>
+    </div>
   );
 }
 
