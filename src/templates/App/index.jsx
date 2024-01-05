d3ee6110d8a3ad0ef239dd04b3b5651e01b1ccb6
Paulo Henrique <p.henriquerm@hotmail.com>
2024-01-04 23:28:43 -0300

    useLayoutEffect

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,80 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
-
-const useAsync = (asyncFunction, shouldRun) => {
-  // const [result, setResult] = useState(null);
-  // const [error, setError] = useState(null);
-  // const [status, setStatus] = useState('idle');
-
-  const [state, setState] = useState({
-    result: null,
-    error: null,
-    status: 'idle'
-  });
-
-  const run = useCallback(async () => {
-    await new Promise((r) => setTimeout(r, 2000));
-
-    setState({
-      result: null,
-      error: null,
-      status: 'pending'
-    });
-
-    await new Promise((r) => setTimeout(r, 2000));
-
-    return asyncFunction()
-      .then(response => {
-        setState({
-          result: response,
-          error: null,
-          status: 'settled'
-        });
-      })
-      .catch(error => {
-        setState({
-          result: null,
-          error: error,
-          status: 'error'
-        });
-      });
-  }, [asyncFunction]);
-
-  useEffect(() => {
-    if(shouldRun) {
-      run();
-    }
-  }, [run, shouldRun])
-
-  return [run, state.result, state.error, state.status];
-};
-
-const fetchData = async () => {
-  // throw new Error('Que chato');
-  await new Promise((r) => setTimeout(r, 2000));
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const json = await data.json();
-  return json;
-};
+import { useLayoutEffect, useRef, useState } from 'react';
 
 function App()  {
-  const [posts, setPosts] = useState(null);
-  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const divRef = useRef();
 
-  if(status === 'idle') {
-    return <pre>idle: Nada executando</pre>;
-  }
+  useLayoutEffect(() => {
+    const now = Date.now();
+    while (Date.now() < now + 1500);
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
 
-  if(status === 'pending') {
-    return <pre>pending: Loading...</pre>;
-  }
+  const handleClick = () => {
+    setCounted((c) => [...c, +c.slice(-1) + 1]);
+  };
 
-  if(status === 'error') {
-    return <pre>error: {error.message}</pre>;
-  }
-
-  if(status === 'settled') {
-    return <pre>settled: {JSON.stringify(result, null, 2)}</pre>;
-  }
+  return (
+    <>
+      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
+      <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+        {counted.map((c) => {
+          return <p key={`c-${c}`}>{c}</p>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default App;
