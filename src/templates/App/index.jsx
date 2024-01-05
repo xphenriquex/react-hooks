9a72d41e7de79fe71b566b1ffdab4427a317158d
Paulo Henrique <p.henriquerm@hotmail.com>
2024-01-04 23:39:35 -0300

    useImperativeHandle e React.forwardRef

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { forwardRef, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react';
 
 function App()  {
   const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
@@ -6,24 +6,47 @@ function App()  {
 
   useLayoutEffect(() => {
     const now = Date.now();
-    while (Date.now() < now + 1500);
-    divRef.current.scrollTop = divRef.current.scrollHeight;
+    while (Date.now() < now + 300);
+    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight;
   });
 
   const handleClick = () => {
     setCounted((c) => [...c, +c.slice(-1) + 1]);
+    divRef.current.handleClick();
   };
 
   return (
     <>
       <button onClick={handleClick}>Count {counted.slice(-1)}</button>
-      <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
-        {counted.map((c) => {
-          return <p key={`c-${c}`}>{c}</p>;
-        })}
-      </div>
+      <DisplayCounted counted={counted} ref={divRef} />
     </>
   );
 };
+
+export const DisplayCounted = forwardRef(function DisplayCounted({ counted }, ref) {
+  const [rand, setRand] = useState('0.24');
+  const divRef = useRef();
+
+  const handleClick = () => {
+    setRand(Math.random().toFixed(2));
+  };
+
+  useImperativeHandle(ref, () => ({
+    handleClick,
+    divRef: divRef.current,
+  }));
+
+  return (
+    <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+      {counted.map((c) => {
+        return (
+          <p onClick={handleClick} key={`c-${c}`}>
+            {c} +++ {rand}
+          </p>
+        );
+      })}
+    </div>
+  );
+});
 
 export default App;
